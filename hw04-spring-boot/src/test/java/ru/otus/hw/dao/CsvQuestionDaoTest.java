4c6715a6aa50b21b7d4916fd670b7813082980b6
SKindrat <skindrat@elar.ru>
2026-03-24 08:47:43 +0300

    [] перевел CsvQuestionDaoTest на SpringBootTest

--- hw04-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw04-spring-boot/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -2,32 +2,30 @@ package ru.otus.hw.dao;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
-
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(properties = "spring.shell.interactive.enabled=false")
 class CsvQuestionDaoTest {
 
-    @Mock
+    @MockitoBean
     private AppProperties properties;
 
+    @Autowired
     private CsvQuestionDao dao;
 
     @BeforeEach
     void setUp() {
         when(properties.getTestFileName()).thenReturn("questions.csv");
-        dao = new CsvQuestionDao(properties);
     }
 
     @Test
